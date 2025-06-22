11b4a677b29b75784c61bedba99804ef7f4784cd
sucka4ai <dosseth11@gmail.com>
2025-06-22 17:49:36 +0100

    Update server.js
    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 const { parse } = require('iptv-playlist-parser');
 const { parseStringPromise } = require('xml2js');
-const { serveHTTP } = require('@stremio/sdk');
+const { serveHTTP } = require('stremio-addon-sdk');
 const dayjs = require('dayjs');
 
 const app = express();
