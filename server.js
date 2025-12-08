78927043822266daaeb0aa37b9519017f2a78711
sucka4ai <dosseth11@gmail.com>
2025-12-08 18:17:48 +0000

    Update server.js

--- server.js
+++ server.js
@@ -489,6 +489,7 @@ app.get('/addon/m3u/catalog/:type/:id.json', async (req, res) => {
 
     const catSet = new Set(items.map(ch => ch.category || "Live"));
     const catArray = ["All", ...Array.from(catSet).sort()];
+    manifest.catalogs[0].extra[0].options = catArray;
 
 
     res.json({ metas });
